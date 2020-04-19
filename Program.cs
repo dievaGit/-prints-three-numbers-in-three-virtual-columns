f559f8e647fbee2657778aa8dd9aaf96488f739b
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-19 13:43:14 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring variables
+            int a = 958456258;
+            double b = 456258.328, c = -654258.328;
+
+            //Requesting data from the user
+            Console.WriteLine("..{0:x}..|..{1:F2}..|..{2:F2}..|", a, b, c);
+
+            Console.ReadKey();
+         
+
+        }
+    }
+}
